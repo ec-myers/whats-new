776e3c1f220807a7d2ed9db959ed903a2eaf8bcf
“Elyse <elysemyers1@gmail.com>
2019-09-30 19:49:12 -0600

    Adds basic setup for NewsArticle component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
-
+const NewsArticle = props => {
+  console.log(props)
+  return (
+    <article className="newsArticle">
+      <img />
+      <h1></h1>
+      <body></body>
+      <footer>
+        <a></a>
+      </footer>
+    </article>
+  )
+}
 export default NewsArticle;
