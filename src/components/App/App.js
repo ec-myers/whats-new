273237b15415ba9b9763adac4df6af0307c62f0c
“Elyse <elysemyers1@gmail.com>
2019-09-30 19:48:25 -0600

    Adds basic setup for App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import NewsContainer from '../NewsContainer/NewsContainer';
+import Menu from '../Menu/Menu';
+import SearchForm from '../SearchForm/SearchForm';
+import NewsArticle from '../NewsArticle/NewsArticle';
 import './App.css';
 
 class App extends Component {
@@ -11,9 +15,12 @@ class App extends Component {
   }
 
   render () {
+    console.log(local)
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <Menu />
+        <SearchForm />
+        <NewsContainer {...this.state} />
       </div>
     );
   }
