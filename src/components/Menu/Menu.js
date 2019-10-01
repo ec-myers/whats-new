ad83090955741a6e827ea8ea1d02acd69c604b47
“Elyse <elysemyers1@gmail.com>
2019-09-30 19:48:42 -0600

    Adds basic setup for Menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = () => {
+  return (
+    <aside className="menu">
+      <h1>What's New</h1>
+        <button>Local News</button>
+        <button>Technology</button>
+        <button>Entertainment</button>
+        <button>Science</button>
+        <button>Health</button>
+    </aside>
+  )
+}
 
 export default Menu;
