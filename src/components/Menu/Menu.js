f3c3b4b99e021c41391eb53edabd762969c74ddf
“Elyse <elysemyers1@gmail.com>
2019-10-02 08:32:14 -0600

    Adds functionality to display specific news for each menu button

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Menu.css'
 
-const Menu = () => {
+const Menu = (props) => {
   return (
     <aside className="menu">
-      <h1 className="menu-title">What's New</h1>
-        <button className="menu-button">Local News</button>
-        <button className="menu-button">Technology</button>
-        <button className="menu-button">Entertainment</button>
-        <button className="menu-button">Science</button>
-        <button className="menu-button">Health</button>
+      <h1 className="menu-title">What's New?</h1>
+        <button className="menu-button" name="local" onClick={event => props.changeNewsFeed(event)}>Local News</button>
+      <button className="menu-button" name="technology" onClick={event => props.changeNewsFeed(event)}>Technology</button>
+      <button className="menu-button" name="entertainment" onClick={event => props.changeNewsFeed(event)}>Entertainment</button>
+      <button className="menu-button" name="science" onClick={event => props.changeNewsFeed(event)}>Science</button>
+      <button className="menu-button" name="health" onClick={event => props.changeNewsFeed(event)}>Health</button>
     </aside>
   )
 }
