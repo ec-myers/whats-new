be2213e929f30309888773d51429aff316087044
“Elyse <elysemyers1@gmail.com>
2019-10-02 08:27:39 -0600

    Adds functionality to include all 5 data sources

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import technology from '../../data/technology';
+import entertainment from '../../data/entertainment';
+import science from '../../data/science';
+import health from '../../data/health';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import Menu from '../Menu/Menu';
 import SearchForm from '../SearchForm/SearchForm';
-import NewsArticle from '../NewsArticle/NewsArticle';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
+    this.local = local;
+    this.technology = technology;
+    this.entertainment = entertainment;
+    this.science = science;
+    this.health = health;
     this.state = {
-      local
+      current: local
     }
+  }
+
+  changeNewsFeed = (event) => {
+    this.setState({ current: this[event.target.name] })
   }
 
   render () {
     console.log(local)
     return (
       <div className="app">
-        <Menu />
+        <Menu changeNewsFeed={this.changeNewsFeed} />
           <div className="content-main">
             <SearchForm />
-            <NewsContainer news={this.state.local} />
+            <NewsContainer news={this.state.current} />
         </div>
       </div>
     );
