c8e12bb61eb973df87021e322ae5ebcd8a8dea79
“Elyse <elysemyers1@gmail.com>
2019-10-03 14:10:34 -0600

    Adds functionality to display searched articles

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-const SearchForm = () => {
-  return (
-    <form className="searchForm">
-      <input className="searchInput" type="text" placeholder="Search for news"></input>
-      <button className="searchButton">Search</button>
-    </form>
-  )
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: ""
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ searchInput: event.target.value });
+  }
+  
+  handleClick = (event) => {
+    event.preventDefault();
+    const searchText = this.state.searchInput.toLowerCase();
+    this.props.displaySearchResults(searchText);
+    this.setState({ searchInput: "" });
+  }
+
+  render() {
+    return (
+      <form className="searchForm">
+        <input 
+          className="searchInput" 
+          name="searchInput" 
+          type="text" 
+          placeholder="Looking for something?" 
+          value={ this.state.searchInput } 
+          onChange={event => this.handleChange(event)}
+        />       
+        <button 
+          className="searchButton" 
+          onClick={event => this.handleClick(event)}>Search
+        </button>
+      </form>
+    )
+  }
 }
 
 
