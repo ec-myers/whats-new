ef7e191673fdd913ac9da4e612bc2cf8f4e66f8b
“Elyse <elysemyers1@gmail.com>
2019-10-06 22:18:54 -0600

    Adds test for snapshot for app component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import local from '../../data/local';
+import technology from '../../data/technology';
+import entertainment from '../../data/entertainment';
+import science from '../../data/science';
+import health from '../../data/health';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should display the local news', () => {
+
+  });
+
 });
